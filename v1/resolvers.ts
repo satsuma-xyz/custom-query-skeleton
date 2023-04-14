9f0bf034fa4fc0d7bdf1b4cf6c2123c29b8bca6d
Andrew Webb <androbwebb@gmail.com>
2023-04-14 16:13:30 +0200

    v1

---
+++ v1/resolvers.ts
@@ -0,0 +1,13 @@
+// You can import types as you wish, but libraries you import will not be available in the VM where these functions are run.
+// The available global libraries are:
+// _ (lodash)
+// moment
+// knex
+// console
+
+// Do not change this export. Satsuma expects a non-default `resolvers` object to be exported from this file.
+export const resolvers = {
+    Query: {
+        // ... - Your resolvers here
+    }
+};
---
+++ v1/typeDefs.ts
@@ -0,0 +1,13 @@
+// You can import types as you wish, but libraries you import will not be available in the VM where these functions are run.
+// The available global libraries are:
+// _ (lodash)
+// moment
+// knex
+// console
+
+// Do not change this export. Satsuma expects a non-default `resolvers` string to be exported from this file.
+const typeDefs = `
+    type Query {
+        ... // Your type definitions here
+    }
+`;
