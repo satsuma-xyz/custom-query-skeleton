a16ac0fc2d5d2838bd7d73f55b3b0b8c8ff653b7
Andrew Webb <androbwebb@gmail.com>
2023-04-28 14:48:45 +0100

    Gql comment

--- v1/typeDefs.ts
+++ v1/typeDefs.ts
@@ -8,6 +8,6 @@
 // Do not change this export. Satsuma expects a non-default `resolvers` string to be exported from this file.
 export const typeDefs = `
     type Query {
-        ... // Your type definitions here
+        # Your type definitions here
     }
 `;
